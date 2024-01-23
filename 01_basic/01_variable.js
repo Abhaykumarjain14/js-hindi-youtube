a0bbafe2adb48ecd7d1ee593dd54c64d088440d0
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-01-23 08:50:28 +0000

    commit varuable change

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "Abhayjain@1234.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+
+accountEmail = "ak@ak.com"
+accountPassword = "1234234"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
