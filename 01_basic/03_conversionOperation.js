50528280444f2568bb6ca682775fb008916ef984
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-01-23 09:48:26 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = null;
+
+// const {score} = req.body
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1
+// let isLoggedIn = "abhay"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 3+3
+
+let strictNumber = String(someNumber)
+console.log(strictNumber);
+console.log(typeof strictNumber);
