62337b4430929be99469d1dbd1aab8c2b4d53adc
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-01-23 12:27:59 +0000

    momory change

--- 01_basic/05_datatype_summery.js
+++ 01_basic/05_datatype_summery.js
@@ -25,20 +25,44 @@ console.log(typeof bigNumber);
 
 // Array , objects,Functions
 
-const id = Symbol('123')
+// const id = Symbol('123')
 
-const heros = ["shaktiman","naagraj","doga"];
-let myObj = {
-    name : "hitesh",
-    age : 22,
+// const heros = ["shaktiman","naagraj","doga"];
+// let myObj = {
+//     name : "hitesh",
+//     age : 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof myFunction);
+
+// console.log(typeof heros);
+
+// console.log(typeof id);
+
+
+// Memory  
+//stack (Primitive) ,Heap Memory(Non - primitive)
+
+let myYoutubename = "abhayjaindotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+let userTwo = userOne
 
-console.log(typeof myFunction);
+userTwo.email = "Abhay@sfad"
 
-console.log(typeof heros);
-
-console.log(typeof id);
+console.log(userOne.email);
+console.log(userTwo.email);
