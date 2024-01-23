c213775499e0dd0627625725ad8358e6c5b2ecc1
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-01-23 10:37:34 +0000

    conversion operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -15,9 +15,47 @@ let valueInNumber = Number(score)
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-
+/*
 let someNumber = 3+3
 
 let strictNumber = String(someNumber)
 console.log(strictNumber);
-console.log(typeof strictNumber);
+console.log(typeof strictNumber)
+
+*/
+
+// ***************************************************************operations***************************************************
+
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = " Abhay"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+"2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
