ce470bb49b3fac43fbd241870d1431a83779803f
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-01-25 09:15:46 +0000

    array02

--- 02_basic/02_Array.js
+++ 02_basic/02_Array.js
@@ -9,3 +9,19 @@ const dc_heros = ["superman","flash","batman"]
 // console.log(allHeros);
 const all_new_Heros = [...marvel_heros,...dc_heros]
 console.log(all_new_Heros);
+
+const another_array = [ 1,2,3,[4,5,6],7, [6,7,[4,5]]]
+// console.log(another_array);
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+// console.log(Array.isArray("Hitesh"))
+
+// console.log(Array.isArray(["Hitesh","abhay"]))
+// console.log(Array.from("Abhay"))
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+    
