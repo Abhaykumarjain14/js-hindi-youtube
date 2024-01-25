eb8238f3a0b6eaa5e444d0634293422d9888a26f
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-01-25 08:55:51 +0000

    arary

---
+++ 02_basic/01_Array.js
@@ -0,0 +1,32 @@
+// Array
+
+const myArr = [0,1,2,3,4,5]
+
+// console.log(myArr[2]);
+
+// const myHeros = ["saktiman","naraag"]
+// console.log(myHeros[1]);
+
+// const myArrn = new Array(1,2,3,4)
+// console.log(myArrn)
+
+//Array methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.shift()
+// myArr.unshift(8)
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("A" , myArr)
+console.log(myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("B",myArr)
+
+console.log("C",myArr)
+
+
+
