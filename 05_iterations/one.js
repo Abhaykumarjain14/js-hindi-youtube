93842c0c88de7fb00561627b8ccea97e65eb9d14
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-02-01 05:34:23 +0000

    controlflow one for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,45 @@
+// for 
+
+// for (let index = 0;index <= 10; index++){
+//     const element = index;
+//     console.log(element);
+// }
+
+// console.log(element)
+
+
+// for (let i = 0; i <= 10; i++){
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is the best number ");
+//     }
+
+//     console.log(element);
+// }
+
+
+// for(let i = 0;i<=10;i++){
+//     console.log(`Outer loop value: ${i}`);
+//     for(let j = 0; j<=10;j++){
+//         console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     }
+// }
+
+// let myArray = ["flash","batman",'saktiman']
+// console.log(myArray.length)
+// for (let index = 0; index < myArray.length; index++){
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+// continue and break
+
+for (let index = 1;index <= 20; index++){
+    if(index == 5){
+        console.log(`Detected 5`);
+        // break
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
