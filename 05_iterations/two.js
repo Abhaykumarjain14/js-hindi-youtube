385e1d22c2694a0387b6e164ca7b98d7e4cac790
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2024-02-01 05:52:32 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while and do while loop
+
+// while (2 == 2){
+
+// }
+
+// let index = 0;
+// while (index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// let myArray = ['flash','batman','superman']
+
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr+1
+// }
+
+let score = 1
+do {
+    console.log(`the score is ${score}`)
+    score++
+}
+while(score <= 10);
